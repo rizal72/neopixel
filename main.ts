6ccf9b719f7bb55ec7d8f8ec4379a8823f127a3c
Riccardo Sallusti <riccardo.sallusti@gmail.com>
2023-03-20 23:49:57 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,6 +9,8 @@ let width = 16
 let height = 16
 strip = neopixel.create(DigitalPin.P1, width * height, NeoPixelMode.RGB)
 EPXDisplay.powerupclear(strip, 10)
+strip.setMatrixWidth(width)
+pins.setMatrixWidth(DigitalPin.P1, width)
 EPXDisplay.play(strip, EPXAnimations.Weather)
 strip.show()
 basic.showString("A")
