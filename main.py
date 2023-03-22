01f504dcb9c0a741b16d9292684b2fcb30d39c50
Riccardo Sallusti <riccardo.sallusti@gmail.com>
2023-03-22 13:39:36 +0100

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -5,36 +5,48 @@ def on_button_pressed_a():
     basic.show_string("A")
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def getCustomMapping(col=16,rows=16):
-    #generate empty col*rows matrix and fill it with 0
-    matrix: List[List[number]] = []
+def getCustomMapping(col: number = 16, rows: number = 16):
+    # generate empty col*rows matrix and fill it with 0
     for r in range(rows):
         matrix.append([])
         for c in range(col):
             matrix[r].append(0)
-        #serial.write_numbers(matrix[r])
-    
+    # serial.write_numbers(matrix[r])
     # Loop through each row and column of the matrix and fill it with an increasing number
     num = 0
-    for i in range(rows):
-        if i % 2 == 0:  # Even-numbered rows
-            for j in range(col-1, -1, -1):
-                matrix[i][j] = num
+    for k in range(rows):
+        if k % 2 == 0:
+            # Even-numbered rows
+            j = col - 1
+            while j > -1:
+                matrix[k][j] = num
                 num += 1
-        else:  # Odd-numbered rows
-            for j in range(rows):
-                matrix[i][j] = num
+                j += -1
+        else:
+            # Odd-numbered rows
+            j = 0
+            while j < rows:
+                matrix[k][j] = num
                 num += 1
-        serial.write_numbers(matrix[i])
-
-strip: neopixel.Strip = None
+                j += 1
+        serial.write_numbers(matrix[k])
 width = 16
 height = 16
+strip: neopixel.Strip = None
+matrix: List[List[number]] = []
 getCustomMapping()
 strip = neopixel.create(DigitalPin.P1, width * height, NeoPixelMode.RGB)
-EPXDisplay.powerupclear(strip, 10)
+# EPXDisplay.powerupclear(strip, 10)
 strip.set_matrix_width(width)
 pins.set_matrix_width(DigitalPin.P1, width)
-EPXDisplay.play(strip, EPXAnimations.WEATHER)
-strip.show()
+# EPXDisplay.play(strip, EPXAnimations.WEATHER)
+showX()
 basic.show_string("A")
+
+def showX():
+    i=0
+    while i <= width - 1:
+        strip.set_matrix_color(i, i, neopixel.colors(NeoPixelColors.RED))
+        strip.set_matrix_color(width - (i + 1), i, neopixel.colors(NeoPixelColors.BLUE))
+        i+=1
+    strip.show()
--- main.ts
+++ main.ts
@@ -6,44 +6,58 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 })
 function getCustomMapping(col: number = 16, rows: number = 16) {
     let j: number;
-    // generate empty col*rows matrix and fill it with 0
-    let matrix : number[][] = []
+    //  generate empty col*rows matrix and fill it with 0
     for (let r = 0; r < rows; r++) {
         matrix.push([])
         for (let c = 0; c < col; c++) {
             matrix[r].push(0)
         }
     }
-    // serial.write_numbers(matrix[r])
+    //  serial.write_numbers(matrix[r])
     //  Loop through each row and column of the matrix and fill it with an increasing number
     let num = 0
-    for (let i = 0; i < rows; i++) {
-        if (i % 2 == 0) {
+    for (let k = 0; k < rows; k++) {
+        if (k % 2 == 0) {
             //  Even-numbered rows
-            for (j = col - 1; j > -1; j += -1) {
-                matrix[i][j] = num
+            j = col - 1
+            while (j > -1) {
+                matrix[k][j] = num
                 num += 1
+                j += -1
             }
         } else {
             //  Odd-numbered rows
-            for (j = 0; j < rows; j++) {
-                matrix[i][j] = num
+            j = 0
+            while (j < rows) {
+                matrix[k][j] = num
                 num += 1
+                j += 1
             }
         }
         
-        serial.writeNumbers(matrix[i])
+        serial.writeNumbers(matrix[k])
     }
 }
 
-let strip : neopixel.Strip = null
 let width = 16
 let height = 16
+let strip : neopixel.Strip = null
+let matrix : number[][] = []
 getCustomMapping()
 strip = neopixel.create(DigitalPin.P1, width * height, NeoPixelMode.RGB)
-EPXDisplay.powerupclear(strip, 10)
+//  EPXDisplay.powerupclear(strip, 10)
 strip.setMatrixWidth(width)
 pins.setMatrixWidth(DigitalPin.P1, width)
-EPXDisplay.play(strip, EPXAnimations.Weather)
-strip.show()
+//  EPXDisplay.play(strip, EPXAnimations.WEATHER)
+showX()
 basic.showString("A")
+function showX() {
+    let i = 0
+    while (i <= width - 1) {
+        strip.setMatrixColor(i, i, neopixel.colors(NeoPixelColors.Red))
+        strip.setMatrixColor(width - (i + 1), i, neopixel.colors(NeoPixelColors.Blue))
+        i += 1
+    }
+    strip.show()
+}
+
